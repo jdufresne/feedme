59bd7032a9841e5ca17452c5b88daa9ccffcd676
Jon Dufresne <jon@jondufresne.org>
2011-11-03 23:06:25 -0700

    Can now successfully refresh a feed to add entries (adds duplicates)

--- feeds/models.py
+++ feeds/models.py
@@ -5,11 +5,17 @@ class Feed(models.Model):
     uri = models.CharField(max_length=255)
     title = models.CharField(max_length=255, blank=True)
 
-    def save(self, *args, **kwargs):
+    def clean(self):
         if not self.title:
             self.title = self.uri
-        super(Feed, self).save(*args, **kwargs)
+        super(Feed, self).clean()
+
+    def __unicode__(self):
+        return self.title
 
 
-class Article(models.Model):
-    feed = models.ForeignKey('Feed')
+class Entry(models.Model):
+    feed = models.ForeignKey('Feed', related_name='entries')
+    author = models.CharField(max_length=255)
+    title = models.CharField(max_length=255)
+    content = models.TextField()
--- feeds/views.py
+++ feeds/views.py
@@ -1,6 +1,7 @@
+import feedparser
 from django.views.decorators.http import require_POST
 from django.shortcuts import get_object_or_404, redirect, render
-from feedme.feeds.models import Feed
+from feedme.feeds.models import Feed, Entry
 
 
 def feed(request, feed_id):
@@ -11,4 +12,18 @@ def feed(request, feed_id):
 @require_POST
 def refresh(request, feed_id):
     feed = get_object_or_404(Feed, pk=feed_id)
+    parsed = feedparser.parse(feed.uri)
+
+    parsed_feed = parsed['feed']
+    feed.title = parsed_feed['title']
+    feed.save()
+
+    for parsed_entry in parsed['entries']:
+        entry = Entry()
+        entry.feed = feed
+        entry.author = parsed_entry['author']
+        entry.title = parsed_entry['title']
+        entry.content = parsed_entry['summary']
+        entry.save()
+
     return redirect('feed', feed_id=feed.id)
